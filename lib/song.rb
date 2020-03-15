1fd4b5b7577addbde93ed6515eb301e78b5fc630
Brandon Gerbasi <brandon.gerbasi@gmail.com>
2020-03-15 19:25:23 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -25,7 +25,6 @@ class Song
 
   def self.create
     song = self.new
-    #@@all << song
     song
   end
   
@@ -47,17 +46,13 @@ class Song
   end
   
   def self.find_or_create_by_name(name)
-      #if self.find_by_name(name) == nil 
-        #self.create_by_name(name)
-      #else
-        #self.find_by_name(name)
-      #end
+      
       self.find_by_name(name) || self.create_by_name(name)
-      #the above statement is saying do this(if it is true) or that (if the first thing is not true and the second thing is true)
+      
   end
   
   def self.alphabetical()
-    #returns all the songs instances in ascending (a-z) alphabetical order.
+    
     @@all.sort_by{|x| x.name}
   end
   
@@ -70,7 +65,7 @@ class Song
   end
   
   def self.create_from_filename(name)
-  #class method should not only parse the filename correctly but should also save the song
+ 
     song = self.new
     song.name = (name.split(" - ")[1].chomp(".mp3"))
     song.artist_name = (name.split(" - ")[0])
